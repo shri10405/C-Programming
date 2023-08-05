3586ddc73937c27753e3f98962353e0fa3b7d847
Shri <sbhardwaj10405@gmail.com>
2023-08-05 10:20:35 +0530

    added product function

--- sum.c
+++ sum.c
@@ -2,8 +2,16 @@
 int sum(int a, int b) {
     return a + b;
 }
+
+int product(int a, int b) {
+    return a * b;
+}
+
 int main() {
     int total = sum(10, 50);
     printf("%d", sum);
+
+    int multiplication = product(10, 50);
+    printf("%d", multiplication);
     return 0;
 }
